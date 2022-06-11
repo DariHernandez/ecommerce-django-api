df0565a1dd7e67a1403c3b2e4ecd7d9ec84cc67b
daridev <hernandezdarifrancisco@gmail.com>
2022-06-11 10:42:13 -0500

    contact forms models

---
+++ contactforms/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User (models.Model):
+    name = models.CharField (max_length=100)
+    api_key = models.CharField (max_length=32)
+    to_emails = models.CharField (max_length=250)
+
+class FromEmail (models.Model):
+    email = models.CharField (max_length=50)
+    password = models.CharField (max_length=16)
+
+class History (models.Model):
+    datetime = models.DateTimeField (auto_now_add=True)
+    user = models.ForeignKey (User, on_delete=models.SET_NULL, null=True)
+    ip = models.CharField (max_length=20)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store',
+    'contactforms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
