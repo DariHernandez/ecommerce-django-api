780f3923342a6e8f1ea9b927b5cf843b7c1fd41e
daridev <hernandezdarifrancisco@gmail.com>
2022-06-16 15:11:38 -0500

    serach brand by name

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ app_name = "store"
 urlpatterns = [
     path ('', views.index, name="index"),
     path ('keagan/', views.keagan_home, name="keagan_home"),
-    path ('keagan/category-products/<int:brand_id>/', views.keagan_category_products, name="keagan_category_products"),
+    path ('keagan/category-products/<str:brand_name>/', views.keagan_category_products, name="keagan_category_products"),
     path ('keagan/product/<int:product_id>/', views.keagan_product, name="keagan_product"),
     path ('keagan/product-new/<int:product_id>/', views.keagan_product_new, name="keagan_product_new")
 ]
