5e9d309935e8c2d394fd8ba541134ee08d21823f
daridev <hernandezdarifrancisco@gmail.com>
2022-06-22 21:48:24 -0500

    remove unused endpoints and tests

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path ('', views.index, name="index"),
     path ('keagan/', views.keagan_home, name="keagan_home"),
     path ('keagan/category-products/<str:brand_name>/', views.keagan_category_products, name="keagan_category_products"),
-    path ('keagan/product/<int:product_id>/', views.keagan_product, name="keagan_product"),
-    path ('keagan/product-new/<int:product_id>/', views.keagan_product_new, name="keagan_product_new"),
     path ('keagan/payment/', views.keagan_payment, name="keagan_payment")
 ]
